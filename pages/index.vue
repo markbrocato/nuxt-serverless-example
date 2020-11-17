e22498106b0851e4d0bb0a59a5ccfe734e32638a
Mark Brocato <mark.brocato@moovweb.com>
2020-11-17 19:20:21 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Blog posts</h1>
+    <p v-if="$fetchState.pending">Fetching post...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <ul v-else>
+      <li v-for="post of posts" :key="post.id">
+        <n-link :to="`/posts/${post.id}`">
+          {{ post.title }}
+        </n-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    this.posts = await fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((posts) => posts.slice(0, 5))
+  },
+  data() {
+    return {
+      posts: null,
+    }
+  },
+}
+</script>
